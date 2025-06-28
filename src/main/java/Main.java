a71a83b63c942b151fb3865939c1357962bb069b
Samruddhi Khairnar <samruddhik2909@gmail.com>
2025-06-28 14:10:10 -0700

    Handle concurrent clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,41 +2,60 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    int port = 6379;
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-          while(true) {
-            byte[] input = new byte[1024];
-            clientSocket.getInputStream().read(input);
-            String inputString = new String(input).trim();
-            System.out.println("Received: " + inputString);
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-          }
 
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-          }
+    try {
+      serverSocket = new ServerSocket(port);
+      // Since the tester restarts your program quite often, setting SO_REUSEADDR
+      // ensures that we don't run into 'Address already in use' errors
+      serverSocket.setReuseAddress(true);
+      // Wait for connection from client.
+
+      while (true) {
+        clientSocket = serverSocket.accept();
+        Socket clientCopy = clientSocket;
+        executorService.submit(() -> handleClient(clientCopy));
+      }
+
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+
+    } finally {
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
         }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
   }
+
+  public static void handleClient(Socket clientSocket) {
+    try{
+      while(true) {
+        byte[] input = new byte[1024];
+        clientSocket.getInputStream().read(input);
+        String inputString = new String(input).trim();
+        System.out.println("Received: " + inputString);
+        clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+      }
+    } catch (Exception e) {
+      System.out.println("IOException: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
 }
